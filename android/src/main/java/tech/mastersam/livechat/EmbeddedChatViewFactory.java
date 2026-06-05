4c3d65e3bdcbcfa42bc5c32377800ce0c47c92b9
iouoiuo <74054674+iouoiuo@users.noreply.github.com>
2026-06-05 12:52:30 +0900

    Update EmbeddedChatViewFactory.java

--- android/src/main/java/tech/mastersam/livechat/EmbeddedChatViewFactory.java
+++ android/src/main/java/tech/mastersam/livechat/EmbeddedChatViewFactory.java
@@ -1,20 +1,30 @@
 package tech.mastersam.livechat;
 
+import android.app.Activity;
 import android.content.Context;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import io.flutter.plugin.common.StandardMessageCodec;
 import io.flutter.plugin.platform.PlatformView;
 import io.flutter.plugin.platform.PlatformViewFactory;
-import io.flutter.plugin.common.EventChannel;
-import io.flutter.plugin.common.StandardMessageCodec;
 
 public class EmbeddedChatViewFactory extends PlatformViewFactory {
+
+    @Nullable
+    private static Activity activity;
 
     public EmbeddedChatViewFactory() {
         super(StandardMessageCodec.INSTANCE);
     }
 
+    static void setActivity(@Nullable Activity activity) {
+        EmbeddedChatViewFactory.activity = activity;
+    }
+
     @Override
-    public PlatformView create(Context context, int id, Object args) {
-        return new EmbeddedChatView(context, args);
+    public PlatformView create(@NonNull Context context, int id, Object args) {
+        return new EmbeddedChatView(context, activity, args);
     }
 }
